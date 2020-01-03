9c91dd1e864899975be98044d383f1f09ee48e18
Higor Diego <higordiegoti@gmail.com>
2020-01-03 19:55:00 -0300

    move sut creation to a factory helper method

--- src/infra/repository/update-acess-token-repository.spec.js
+++ src/infra/repository/update-acess-token-repository.spec.js
@@ -2,7 +2,7 @@ const { connect, disconnect } = require('../helpers/mongo-helper')
 const MissingParamError = require('../../utils/errors/missing-param-error')
 let db
 
-class UpdateAcessTokenRepository {
+class UpdateAccessTokenRepository {
   constructor (userModel) {
     this.userModel = userModel
   }
@@ -13,6 +13,12 @@ class UpdateAcessTokenRepository {
     const result = await this.userModel.updateOne({ _id: userId }, { $set: { accessToken } })
     return result
   }
+}
+
+const makeSut = () => {
+  const userModel = db.collection('users')
+  const sut = new UpdateAccessTokenRepository(userModel)
+  return { sut, userModel }
 }
 
 describe('UpdateAcessToken Repository', () => {
@@ -28,11 +34,16 @@ describe('UpdateAcessToken Repository', () => {
     await db.collection('users').deleteMany()
   })
   test('should update the user with the given acessToken', async () => {
-    const userModel = db.collection('users')
-    const sut = new UpdateAcessTokenRepository(userModel)
+    const { sut, userModel } = makeSut()
     const fakeUser = await userModel.insertOne({ email: 'valid@mail.com', name: 'any_name', age: 50, state: 'any_state', password: 'hashed_password' })
     await sut.update(fakeUser.ops[0]._id, 'valid_token')
     const updateFakeUser = await userModel.findOne({ _id: fakeUser.ops[0]._id })
     expect(updateFakeUser.accessToken).toBe('valid_token')
+  })
+  test('Should throw if no userModel is provided', async () => {
+    const { sut, userModel } = makeSut()
+    const fakeUser = await userModel.insertOne({ email: 'valid@mail.com', name: 'any_name', age: 50, state: 'any_state', password: 'hashed_password' })
+    const promise = sut.update(fakeUser.ops[0]._id, 'valid_token')
+    expect(promise).rejects.toThrow()
   })
 })
