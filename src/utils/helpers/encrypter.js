999ec91efcb9121c3be3490723f768a89fc50213
Higor Diego <higordiegoti@gmail.com>
2020-01-03 15:25:23 -0300

    fix bug params compare

--- src/utils/helpers/encrypter.js
+++ src/utils/helpers/encrypter.js
@@ -2,8 +2,8 @@ const bcrypt = require('bcrypt')
 const MissingParamError = require('../errors/missing-param-error')
 module.exports = class Encrypter {
   async compare (value, hash) {
-    if (!value) return MissingParamError('value')
-    if (!hash) return MissingParamError('hash')
+    if (!value) throw new MissingParamError('value')
+    if (!hash) throw new MissingParamError('hash')
     const isValid = await bcrypt.compare(value, hash)
     return isValid
   }
