b908bb0e2e541e0de64aa4e0694857954aa1c51e
Higor Diego <higordiegoti@gmail.com>
2020-01-04 17:41:35 -0300

    not coverage test folder main/*

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  collectCoverageFrom: ['**/src/**/*.js'],
+  collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/**.js'],
   preset: '@shelf/jest-mongodb'
 }
