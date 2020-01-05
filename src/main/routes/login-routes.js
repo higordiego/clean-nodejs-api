c21067f4903a2e098e2beb29cbb8e37cec9b464c
Higor Diego <higordiegoti@gmail.com>
2020-01-05 11:39:13 -0300

    create adapter express request

--- src/main/routes/login-routes.js
+++ src/main/routes/login-routes.js
@@ -1,5 +1,7 @@
 const LoginRouter = require('../compose/login-router-compose')
 
+const ExpressRouterAadapter = require('../adapters/express-router-adapter')
+
 module.exports = async router => {
-  router.post('/login', LoginRouter)
+  router.post('/login', ExpressRouterAadapter.adapt(LoginRouter))
 }
