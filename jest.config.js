2693c3c354fb8363502a396a7e23509879755b49
Higor Diego <higordiegoti@gmail.com>
2020-01-06 08:39:43 -0300

    mock bcrypt import file

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  collectCoverageFrom: ['**/src/**/*.js'],
-  // collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/**'],
-  modulePathIgnorePatterns: ['src/main'],
+  // collectCoverageFrom: ['**/src/**/*.js'],
+  collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/**'],
+  // modulePathIgnorePatterns: ['src/main'],
   preset: '@shelf/jest-mongodb'
 }
